d17cad386a1fd56e5ebb36e3b41d0724e3ac34a5
Jimmy Catalano <jimmy.catalanoperso@gmail.com>
2024-01-11 18:18:32 +0100

    Route modification

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,9 +3,11 @@ import { IndexPage } from "./pages/IndexPage";
 import { LoginPage } from "./pages/LoginPage";
 import { Layout } from "./Layout";
 import { RegisterPage } from "./pages/RegisterPage";
-import { AccountPage } from "./pages/AccountPage";
+import { ProfilePage } from "./pages/ProfilePage";
 import axios from "axios";
 import { UserContextProvider } from "./UserContext";
+import { PlacePage } from "./pages/PlacePage";
+import { PlacesFormPage } from "./pages/PlacesFormPage";
 
 axios.defaults.baseURL = "http://localhost:4000";
 axios.defaults.withCredentials = true;
@@ -18,8 +20,9 @@ function App() {
         <Route index element={<IndexPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="/account/:subpage?" element={<AccountPage/>} />
-        <Route path="/account/:subpage/:action" element={<AccountPage/>} />
+        <Route path="/account" element={<ProfilePage/>} />
+        <Route path="/account/places" element={<PlacePage/>} />
+        <Route path="/account/places/:id" element={<PlacesFormPage/>} />
       </Route>
     </Routes>
     </UserContextProvider>
