aa52d38daeaa192d104f86581b65f9b8562a335f
Jimmy Catalano <jimmy.catalanoperso@gmail.com>
2024-02-19 19:33:18 +0100

    Comment out express.static

--- api/index.js
+++ api/index.js
@@ -19,7 +19,7 @@ const jwtSecret = "faseiojgzpOjzfzjfzzfjzfzfzheoitj";
 
 app.use(express.json());
 app.use(cookieParser());
-app.use("/uploads", express.static(__dirname + "/uploads"));
+// app.use("/uploads", express.static(__dirname + "/uploads"));
 
 app.use(
   cors({
